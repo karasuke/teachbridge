d27aca2c411308840e8d60f21a8e3b0e19433782
Boris <boris.zepeda@aurusjoyeria.cl>
2024-08-16 22:21:15 -0400

    refactor - se repara conexion con back

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,11 +1,9 @@
+import React from "react";
 import { Col, Container, Image, Row, Button, Navbar } from "react-bootstrap";
-
 import "./landing.css";
 import { RoutesDirections } from "../data/libraries/Routes";
 import { useNavigate } from "react-router-dom";
 import { ScreenSize, useScreenSize } from "../hooks/useScreenSize";
-import React from "react";
-import { MDBFooter, MDBContainer, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 
 export const LandingPage = () => {
   const navigate = useNavigate(); // Usa el hook useNavigate
@@ -21,8 +19,22 @@ export const LandingPage = () => {
 
   const TITLE_TEXT = "TeachBridge";
   const CLASES_TEXT = "Clases";
+  const CREATOR_TEXT = "Creadores";
   const ADMINISTRATOR_TEXT = "Administrador";
   const INFO_CONTAINER_BUTTON_TEXT = "¡Comienza a aprender!";
+  const QUALITY_EDUCATION_TEXT = "Educación de calidad a un clic.";
+  const ABOUT_US_TEXT = "Sobre Nosotros";
+  const QUALITY_DETAILS_TEXT =
+    "¿Quieres aprender de forma divertida y efectiva? ¡Explora nuestros cursos gratis y descubre nuestro contenido educativo personalizado para alcanzar tus metas educativas!";
+  const ONLINE_EDUCATION_TEXT = "La educación en línea, más cerca de ti.";
+  const ABOUT_US_DETAILS_TEXT =
+    "Experimenta una forma de aprendizaje personalizada y accesible desde cualquier lugar. ¡Descubre cómo nuestros videos pueden facilitar la educación en casa!";
+  const SERVICE_TITLE_TEXT = "Nuestro Servicio";
+  const SERVICE_SUBTITLE_TEXT = "Aprendizaje a tu medida";
+  const SERVICE_DETAILS_TEXT =
+    "¿Quieres aprender de forma divertida y efectiva? ¡Explora nuestros cursos gratis y descubre nuestro contenido educativo personalizado para alcanzar tus metas educativas!";
+  const COPYRIGHT_TEXT = " © 2024 Copyright: TeachBridge";
+
   return (
     <Container fluid className="p-0">
       {isMobile ? (
@@ -57,6 +69,7 @@ export const LandingPage = () => {
                     >
                       {CLASES_TEXT}
                     </Button>
+                    
 
                     <Button
                       className="button-color  "
@@ -82,7 +95,7 @@ export const LandingPage = () => {
               <Col lg={8} className="m-auto">
                 <Col className="mb-2 text-center text-lg-start">
                   <span className={`${isMobile ? "h5" : "h2"} text-primary-3 `}>
-                    {"Sobre Nosotros"}
+                    {ABOUT_US_TEXT}
                   </span>
                 </Col>
 
@@ -91,7 +104,7 @@ export const LandingPage = () => {
                 </Col>
 
                 <Col className="mt-4 text-center text-lg-start">
-                  <Button className={`btn-secondary-text-small px-5`}>
+                  <Button className={`bg-primary-1 px-5`}>
                     {INFO_CONTAINER_BUTTON_TEXT}
                   </Button>
                 </Col>
@@ -109,7 +122,7 @@ export const LandingPage = () => {
               <Col lg={8} className="m-auto">
                 <Col className="mb-2 text-center text-lg-start">
                   <span className={`${isMobile ? "h5" : "h2"} text-primary-3 `}>
-                    {"Sobre Nosotros"}
+                    {ABOUT_US_TEXT}
                   </span>
                 </Col>
 
@@ -128,9 +141,9 @@ export const LandingPage = () => {
         </>
       ) : (
         <>
-          <Row className="d-flex g-0 my-3">
+          <Row className="d-flex g-0 my-3 header  ">
             <Col lg={12}>
-              <Navbar className="navbar navbar-dark p-0 ">
+              <Navbar className="navbar navbar-dark p-0  ">
                 <Col className="d-flex align-items-start  ">
                   <Col className="px-5">
                     <a className="navbar-brand px-2" href="/">
@@ -154,6 +167,12 @@ export const LandingPage = () => {
                       {CLASES_TEXT}
                     </button>
                     <button
+                      className="bg-nav-buttons text-secondary-1  p-large button-color my-2 fw-bold "
+                      type="submit"
+                    >
+                      {CREATOR_TEXT}
+                    </button>
+                    <button
                       className="bg-nav-buttons text-secondary-1 p-large button-color my-2 fw-bold "
                       type="submit"
                       onClick={handleAdminClick}
@@ -166,29 +185,26 @@ export const LandingPage = () => {
             </Col>
           </Row>
 
-          <Row className="g-0  ">
+          <Row className="g-0 my-5 py-5  ">
             <Col
               lg={6}
-              className="d-flex p-0 justify-content-end align-items-center "
+              className="d-flex justify-content-center align-items-center "
             >
-              <Col lg={10}>
-                <Col className="mb-2 text-center text-lg-start">
+              <Col lg={7}>
+                <Col className="text-start mb-3 ">
                   <span className={`${isMobile ? "h5" : "h1"} text-primary-1 `}>
-                    {"Educación de calidad a un clic."}
+                    {QUALITY_EDUCATION_TEXT}
                   </span>
                 </Col>
 
-                <Col className="text-center text-lg-start pb-4">
+                <Col className="text-start ">
                   <span className="text-secondary-2 h5 ">
-                    {
-                      "¿Quieres aprender de forma divertida y efectiva? ¡Explora nuestros cursos gratis y descubre nuestro contenido educativo personalizado para alcanzar tus metas educativas!"
-                    }
+                    {QUALITY_DETAILS_TEXT}
                   </span>
                 </Col>
-
                 <Col className="mt-4 text-center text-lg-start">
                   <Button
-                    className={`bg-secondary-2 border-0 rounded rounded-3 px-2`}
+                    className={`bg-secondary-2 shadow border-0 rounded rounded-3 px-2 button-hover-effect`}
                   >
                     {INFO_CONTAINER_BUTTON_TEXT}
                   </Button>
@@ -199,35 +215,72 @@ export const LandingPage = () => {
               <Col xs={10} lg={8} className="m-auto">
                 <Image
                   fluid
-                  src={"/src/assets/images/img3.png"}
+                  src={"/src/assets/images/1img.png"}
                   alt={``}
                   width={400}
                 />
               </Col>
             </Col>
           </Row>
-          <Row className="g-0 mb-5 pb-5 ">
+          <Row className="g-0  mt-5 ">
+            <Col lg={6} className="mb-4 m-lg-0">
+              <Col xs={10} lg={8} className="m-auto">
+                <Image
+                  fluid
+                  src={"/src/assets/images/2img.png"}
+                  alt={``}
+                  width={320}
+                />
+              </Col>
+            </Col>
             <Col
               lg={6}
-              className="d-flex p-0 justify-content-end align-items-center "
+              className="d-flex justify-content-center align-items-center "
             >
-              <Col lg={11}>
-                <Col className="mb-2 px-5 mx-4">
+              <Col lg={12}>
+                <Col lg={6}>
+                  <Col className="text-start mb-3 ">
+                    <span
+                      className={`${isMobile ? "h5" : "h1"} text-primary-1 `}
+                    >
+                      {ABOUT_US_TEXT}
+                    </span>
+                  </Col>
+                  <Col className="mb-2">
+                    <span className="p-large fw-bold ">
+                      {ONLINE_EDUCATION_TEXT}
+                    </span>
+                  </Col>
+
+                  <Col className="text-start ">
+                    <span className="text-secondary-2 h5 ">
+                      {ABOUT_US_DETAILS_TEXT}
+                    </span>
+                  </Col>
+                </Col>
+              </Col>
+            </Col>
+          </Row>
+          <Row className="g-0   ">
+            <Col
+              lg={6}
+              className="d-flex justify-content-center align-items-center "
+            >
+              <Col lg={7}>
+                <Col className="text-start mb-3 ">
                   <span className={`${isMobile ? "h5" : "h1"} text-primary-1 `}>
-                    {"Sobre Nosotros"}
+                    {SERVICE_TITLE_TEXT}
                   </span>
                 </Col>
-                <Col className="mb-5 px-5 mx-4">
-                  <span className="text-secondary-1 fw-bold h3">
-                    La educación en línea, más cerca de ti.
+                <Col>
+                  <span className="p-large fw-bold">
+                    {SERVICE_SUBTITLE_TEXT}
                   </span>
                 </Col>
 
-                <Col className=" bg-tiktok  rounded rounded-4 p-5 mx-5">
-                  <span className="text-primary-5 h5  ">
-                    {
-                      "Experimenta una forma de aprendizaje personalizada y accesible desde cualquier lugar. ¡Descubre cómo nuestros videos pueden facilitar la educación en casa!"
-                    }
+                <Col className="text-start ">
+                  <span className="text-secondary-2 h5 ">
+                    {SERVICE_DETAILS_TEXT}
                   </span>
                 </Col>
               </Col>
@@ -236,56 +289,21 @@ export const LandingPage = () => {
               <Col xs={10} lg={8} className="m-auto">
                 <Image
                   fluid
-                  src={"/src/assets/images/img2.png"}
+                  src={"/src/assets/images/3img.png"}
                   alt={``}
                   width={400}
                 />
               </Col>
             </Col>
           </Row>
-          <Row className="g-0 mb-5 pb-5 ">
-            
-              <Col lg={10} className="px-5 mx-4">
-                <Col className="mb-2 px-5 mx-4">
-                  <span className={`${isMobile ? "h5" : "h1"} text-primary-1 `}>
-                    {"Nuestro Servicio"}
-                  </span>
-                </Col>
-                <Col className="mb-5 px-5 mx-4" >
-                  <span className="text-secondary-1 fw-bold h3">
-                    Aprendizaje a tu medida
-                  </span>
-                </Col>
-                <Col lg={11}
-                  className="bg-tiktok rounded rounded-4 p-5 mx-5 d-flex flex-row align-items-center"
-                  
-                >
-                  <Col lg={7} className="text-primary-5 h5">
-                    {
-                      "Nuestros videos educativos ofrecen una experiencia de aprendizaje personalizada y flexible. Cada niño podrá avanzar a su propio ritmo, accediendo a las lecciones cuantas veces estime necesario."
-                    }
-                  </Col>
-                  <Col className="d-flex justify-content-end">
-                    <Image
-                      fluid
-                      src={"/src/assets/images/image2.png"}
-                      alt={``}
-                      className="absolute-image"
-                    />
-                  </Col>
-                </Col>
-              </Col>
-        
-          </Row>
+
           <Row className="g-0 pt-5 my-5">
             <Col className="text-center p-0 footer">
               <Col className="text-center text-white bg-secondary-2">
                 <Col className="p-4 "></Col>
 
                 <Col className="text-center p-3 bg-secondary-1">
-                  <span className="p-regular">
-                    © 2024 Copyright: TeachBridge
-                  </span>
+                  <span className="p-regular">{COPYRIGHT_TEXT}</span>
                 </Col>
               </Col>
             </Col>
